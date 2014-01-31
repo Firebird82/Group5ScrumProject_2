a84fa29aa5aaa99fd861122796ad1cefda954999
bos79 <bjorn.sodergren@comhem.se>
2014-01-31 13:47:45 +0100

    björn
    ändringar

--- Group5ScrumProject/Controllers/HomeController.cs
+++ Group5ScrumProject/Controllers/HomeController.cs
@@ -26,7 +26,6 @@ namespace Group5ScrumProject.Controllers
             {
                 Session["User"] = loggedInUser;
                 ViewBag.User = loggedInUser;
-                //hej
                 return View();
             }
             Session.Clear();
@@ -38,7 +37,9 @@ namespace Group5ScrumProject.Controllers
         {
             Session.Clear();
             ViewBag.User = null;
-            return View("index");
+            return View("Login");
+
+            //Björn
         }
     }
 }
