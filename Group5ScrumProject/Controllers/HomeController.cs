c8473b9362dd5d38d4cc5dffb690964a6592d760
Firebird82 <david.sebela@gmail.com>
2014-01-31 09:27:33 +0100

    Upload
    First Upload

---
+++ Group5ScrumProject/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Group5ScrumProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult Login(string tbxName, string tbxPassword)
+        {
+            if (Session["User"] != null)
+                return View();
+            tbUser loggedInUser = (from f in db.tbUsers
+                     where f.sUserLoginName == tbxName && f.sUserPassword == tbxPassword
+                     select f).FirstOrDefault();
+
+            if (loggedInUser != null)
+            {
+                Session["User"] = loggedInUser;
+                ViewBag.User = loggedInUser;
+                return View();
+            }
+            Session.Clear();
+            if(tbxName != null && tbxPassword != null)
+                ViewBag.Message = "Felaktigt användarnamn eller lösenord";
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            ViewBag.User = null;
+            return View("index");
+        }
+    }
+}
